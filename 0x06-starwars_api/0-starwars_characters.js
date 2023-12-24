835bfff50bc3abe4bf91291dfc11b778eabad09d
Joseph-Lukudu-Nelson <jkudus2022@gmail.com>
2023-12-24 05:38:46 -0800

    print a character

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// Script that prints all characters of a Star Wars movie
+// The first position argument is the Movie ID - example: 3 = “Return of the Jedi”
+// Display one character name by line in the same order of
+// the list “characters” in the /films/ response.
+// endpoint: https://swapi-api.alx-tools.com/api/films/:id
+
+
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
+    }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
